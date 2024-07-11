29a6e054497a7313dc9e06bd57018a09d2f383e7
Natan Wojcik <zlac473@rhul.ac.uk>
2024-07-11 14:51:01 +0100

    first commit

---
+++ components/navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const NavigationBarComponent = () => {
+
+  return (
+    <nav className="grid grid-cols-2  w-screen z-10 text-black fixed top-0">
+      
+    </nav>
+  );
+}
---
+++ src/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavigationBarComponent } from '../components/navbar';
+
+export default function Home() {
+  return (
+    <main className="text-center h-screen flex">
+    <NavigationBarComponent/>
+    </main>
+  );
+};
