8a9f97a5dc51db7457f5e26ae740e85650f87c36
Natan Wojcik <zlac473@rhul.ac.uk>
2024-07-11 15:52:55 +0100

    feat: added custom colours/theme

--- src/home.tsx
+++ src/home.tsx
@@ -3,7 +3,7 @@ import { NavigationBarComponent } from '../components/navbar';
 
 export default function Home() {
   return (
-    <main className="text-center h-screen flex">
+    <main className="text-center h-screen flex bg-cream-100">
     <NavigationBarComponent/>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
+    },
+    colors: {
+      ...require("tailwindcss/colors"),
+      limegreen: {
+        600: `#B9FF66`,
+      },
+      darkgreen: {
+        100: `#00674f`,
+      },
+      cream : {
+        100: `#fcfbe6`
+      },
+    },
   },
   plugins: [],
 }
