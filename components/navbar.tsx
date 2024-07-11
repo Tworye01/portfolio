bd9b419ea2d359da8db89daa12128c4621944efe
Natan Wojcik <zlac473@rhul.ac.uk>
2024-07-11 15:52:33 +0100

    feat: added nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-export const NavigationBarComponent = () => {
 
+
+export const NavigationBarComponent = () => {
   return (
-    <nav className="grid grid-cols-2  w-screen z-10 text-black fixed top-0">
-      
+    <nav className="bg-cream-100 w-full fixed z-10 font-rubik">
+      <div className="container mx-auto py-4 flex justify-center items-center">
+        <div className="text-xl space-x-12 font-bold flex">
+          <a href="/home" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-2 rounded-full transition-colors duration-300">home</a>
+          <a href="/projects" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-2 rounded-full transition-colors duration-300">projects</a>
+          <a href="/contact" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-2 rounded-full transition-colors duration-300">contact</a>
+        </div>
+      </div>
     </nav>
   );
 }
