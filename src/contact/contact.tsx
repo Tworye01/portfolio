7799a70368a85f846f718b4d739a58b5fe0f2f25
Natan Wojcik <zlac473@rhul.ac.uk>
2024-08-23 12:44:24 +0100

    feat: added router functionality

---
+++ src/contact/contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavigationBarComponent } from '../components/navbar';
+
+export default function Contacts() {
+  return (
+    <main className="h-screen flex flex-col bg-cream-100">
+       <NavigationBarComponent />
+    </main>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './home.tsx'
-import Projects from './projects/projects.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './home.tsx';
+import Projects from './projects/projects.tsx';
+import './index.css';
+import Contacts from './contact/contact.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
-    <Projects />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contacts/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
