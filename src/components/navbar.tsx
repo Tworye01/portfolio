7d48d08aa42c074cdd6061eebcf4f52d0b2619fc
Natan Wojcik <zlac473@rhul.ac.uk>
2024-08-23 12:02:06 +0100

    feat: slight visual changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export const NavigationBarComponent = () => {
   return (
-    <nav className="bg-cream-100 w-full fixed z-10 font-rubik">
+    <nav className="bg-transparent w-full fixed z-10 font-rubik">
       <div className="container mx-auto py-4 flex justify-center items-center">
         <div className="text-xl space-x-12 font-bold flex">
           <a href="/" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-2 rounded-full transition-colors duration-300">home</a>
--- src/home.tsx
+++ src/home.tsx
@@ -3,8 +3,9 @@ import { NavigationBarComponent } from './components/navbar';
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col bg-cream-100">
+    <main className="h-screen flex flex-col ">
        <NavigationBarComponent />
+       <div className="absolute top-0 -z-10 h-full w-full bg-cream-100"><div className="absolute bottom-auto left-auto top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-darkgreen-100 opacity-20 blur-[80px]"></div></div>
        <div className="flex flex-1 justify-center items-center">
         <div className="text-center">
           <p className="text-8xl font-bold text-darkgreen-100 font-rubik">
