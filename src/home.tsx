e1b0b53004538ef3a7672fc7a7b3ced544f7a06b
Natan Wojcik <zlac473@rhul.ac.uk>
2024-08-23 12:14:01 +0100

    fix: fixed background

--- src/home.tsx
+++ src/home.tsx
@@ -5,16 +5,19 @@ export default function Home() {
   return (
     <main className="h-screen flex flex-col ">
        <NavigationBarComponent />
-       <div className="absolute top-0 -z-10 h-full w-full bg-cream-100"><div className="absolute bottom-auto left-auto top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-darkgreen-100 opacity-20 blur-[80px]"></div></div>
+       <div className="absolute top-0 -z-10 h-full w-full bg-cream-100">
+          <div className="absolute top-1/2 left-1/2 h-[900px] w-[100px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-darkgreen-100 opacity-20 blur-[80px] transform rotate-[45deg]"></div>
+      </div>
+
        <div className="flex flex-1 justify-center items-center">
-        <div className="text-center">
-          <p className="text-8xl font-bold text-darkgreen-100 font-rubik">
-            hi, i'm natan
-          </p>
-          <p className="text-4xl font-bold text-darkgreen-100 font-rubik">
-            current student at rhul
-          </p>
-        </div>
+          <div className="text-center">
+            <p className="text-8xl font-bold text-darkgreen-100 font-rubik">
+              hi, i'm natan
+            </p>
+            <p className="text-4xl font-bold text-darkgreen-100 font-rubik">
+              current student at rhul
+            </p>
+          </div>
        </div>
     </main>
   );
