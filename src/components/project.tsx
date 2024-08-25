6522b2c8cc75027759a2a81200519a68df94ce82
Natan Wojcik <zlac473@rhul.ac.uk>
2024-08-25 18:14:32 +0100

    fix: fixed image sizing

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -9,13 +9,13 @@ interface ProjectCardProps {
 
   export const ProjectCardComponent: React.FC<ProjectCardProps> = ({ image, title, description, link }) => {
     return (
-      <a href={link} target="_blank" className="w-80 outline outline-5 h-96 outline-darkgreen-100 bg-cream-100 rounded-lg shadow-xl transition-transform transform hover:scale-105">
-        <div className="w-full h-48">
+      <a href={link} target="_blank" className="w-80 outline outline-5 h-86 overflow-hidden outline-darkgreen-100 bg-cream-100 rounded-lg shadow-xl transition-transform transform hover:scale-105">
+        <div className="w-80 h-80">
           <img className="w-full h-full object-cover" src={image} alt={title} />
         </div>
         <div className="p-4 flex-grow">
-          <h3 className="text-xl font-bold text-darkgreen-100 mb-2">{title}</h3>
-          <p className="text-gray-700">{description}</p>
+          <h3 className="text-2xl drop-shadow-2xl font-bold text-darkgreen-100 mb-2 font-rubik">{title}</h3>
+          <p className="text-gray-700 text-lg">{description}</p>
         </div>
       </a>
     );
