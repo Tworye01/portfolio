da619e39a508f9f6791996eecf43fd812e64e0f5
Natan Wojcik <zlac473@rhul.ac.uk>
2024-08-27 17:59:27 +0100

    fix: tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './home.tsx';
-import Projects from './projects/projects.tsx';
+import Home from './home';
+import Projects from './projects/projects';
 import './index.css';
-import Contacts from './contact/contact.tsx';
+import Contacts from './contact/contact';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
