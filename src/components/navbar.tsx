0248ea9b5b7331eae36dbc71c7f16b3ac98266f5
Natan Wojcik <zlac473@rhul.ac.uk>
2024-08-27 19:59:53 +0100

    fix: routing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
@@ -7,9 +8,9 @@ export const NavigationBarComponent = () => {
     <nav className="bg-transparent w-full fixed z-10 font-rubik ">
       <div className="container mx-auto py-4 flex justify-center items-center">
         <div className="text-3xl space-x-12 font-bold flex rounded-full px-6 py-1">
-          <a href="/portfolio/" className="text-darkgreen-100 hover:bg-darkgreen-100  hover:text-cream-100 p-3 shadow-md bg-cream-100 rounded-full transition-colors duration-300">home</a>
-          <a href="/portfolio/projects" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-3 shadow-md bg-cream-100  rounded-full transition-colors duration-300">projects</a>
-          <a href="/portfolio/contact" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-3 shadow-md bg-cream-100 rounded-full transition-colors duration-300">contact</a>
+          <Link to="/" className="text-darkgreen-100 hover:bg-darkgreen-100  hover:text-cream-100 p-3 shadow-md bg-cream-100 rounded-full transition-colors duration-300">home</Link>
+          <Link to="/projects" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-3 shadow-md bg-cream-100  rounded-full transition-colors duration-300">projects</Link>
+          <Link to="/contact" className="text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100 p-3 shadow-md bg-cream-100 rounded-full transition-colors duration-300">contact</Link>
         </div>
       </div>
     </nav>
