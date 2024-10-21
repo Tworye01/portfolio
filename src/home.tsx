a4a11b5453b02895384891df5b298b6819fe9cd3
Natan Wojcik <zlac473@rhul.ac.uk>
2024-10-21 16:36:49 +0100

    fix: home text

--- src/home.tsx
+++ src/home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="absolute top-1/2 left-1/2 h-[600px] w-[90%] md:h-[1000px] md:w-[1700px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-darkgreen-100 opacity-20 blur-[80px]"></div>
       </div>
 
-      <div className="flex flex-1 justify-center items-center z-10 pt-32">
+      <div className="flex flex-1 justify-center items-center z-10 pt-64">
         <div className="text-center">
           <p className="text-5xl sm:text-8xl md:text-9xl font-bold text-darkgreen-100 drop-shadow-2xl font-rubik">
             hi, i'm natan
