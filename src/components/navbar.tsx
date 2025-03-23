9365d86bca7a65b3a4953c6e57858733d8326851
Natan Wojcik <zlac473@rhul.ac.uk>
2025-03-23 22:25:23 +0000

    fix: fixed navbar again

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,25 +11,31 @@ export const NavigationBarComponent = () => {
         <div className="font-bold flex space-x-6 md:space-x-12">
           <Link
             to="/"
-            className={`text-darkgreen-100 text-lg md:text-3xl p-3 md:p-3 shadow-md bg-cream-100 rounded-full border-4 border-darkgreen-100 transition-colors duration-300 ${
-              isActive('/') ? 'text-cream-100 bg-darkgreen-100 ' : 'hover:bg-darkgreen-100 hover:text-cream-100'
-            }`}
+            className={`text-lg md:text-3xl p-3 md:p-3 shadow-md rounded-full border-4 border-darkgreen-100 transition-colors duration-300 
+              ${isActive('/') 
+                ? 'bg-darkgreen-100 text-cream-100'
+                : 'bg-cream-100 text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100' 
+              }`}
           >
             home
           </Link>
           <Link
             to="/projects"
-            className={`text-darkgreen-100 text-lg md:text-3xl p-3 md:p-3 shadow-md bg-cream-100 rounded-full border-4 border-darkgreen-100 transition-colors duration-300 ${
-              isActive('/projects') ? 'bg-darkgreen-100 text-cream-100' : 'hover:bg-darkgreen-100 hover:text-cream-100'
-            }`}
+            className={`text-lg md:text-3xl p-3 md:p-3 shadow-md rounded-full border-4 border-darkgreen-100 transition-colors duration-300 
+              ${isActive('/') 
+                ? 'bg-darkgreen-100 text-cream-100'
+                : 'bg-cream-100 text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100' 
+              }`}
           >
             projects
           </Link>
           <Link
             to="/contact"
-            className={`text-darkgreen-100 text-lg md:text-3xl p-3 md:p-3 shadow-md bg-cream-100 rounded-full border-4 border-darkgreen-100 transition-colors duration-300 ${
-              isActive('/contact') ? 'bg-darkgreen-100 text-cream-100' : 'hover:bg-darkgreen-100 hover:text-cream-100'
-            }`}
+            className={`text-lg md:text-3xl p-3 md:p-3 shadow-md rounded-full border-4 border-darkgreen-100 transition-colors duration-300 
+              ${isActive('/') 
+                ? 'bg-darkgreen-100 text-cream-100'
+                : 'bg-cream-100 text-darkgreen-100 hover:bg-darkgreen-100 hover:text-cream-100' 
+              }`}
           >
             contact
           </Link>
