f88ed8415c7178919196a71867f7e6b75e67a7ac
Natan Wojcik <zlac473@rhul.ac.uk>
2025-03-23 21:07:20 +0000

    feat: added new project

--- src/projects/projects.tsx
+++ src/projects/projects.tsx
@@ -5,15 +5,15 @@ import { ProjectCardComponent } from '../components/project';
 export default function Projects() {
   return (
     <main className="overflow-y-auto relative flex flex-col items-center bg-cream-100 h-screen">
-       <div className="absolute top-1/2 left-1/2 h-[600px] w-[90%] md:h-[1000px] md:w-[1700px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-darkgreen-100 opacity-20 blur-[80px]"></div>
+       <div className="absolute top-1/2 left-1/2 h-[600px] w-[90%] md:h-[1900px] md:w-[1700px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-darkgreen-100 opacity-20 blur-[80px]"></div>
 
       <div className="flex-grow mt-32 flex flex-col items-center">
-        <h3 className="p-2 text-2xl drop-shadow-2xl font-bold font-rubik text-darkgreen-100 mb-2 text-center">
+        <h3 className="p-2 text-2xl drop-shadow-2xl font-bold font-rubik text-darkgreen-100 mb-8 text-center">
           Click projects to find out more!
         </h3>
       </div>
 
-      <div className="flex flex-col md:flex-row gap-8 mb-64">
+      <div className="flex flex-wrap justify-center gap-8 mb-64 max-w-[90%]">
         <ProjectCardComponent
           image={'./assets/tuner.PNG'}
           title={'Tuner'}
@@ -38,7 +38,12 @@ export default function Projects() {
           description={'Cool website written in ReactTS with TailwindCSS'}
           link={'https://tworye01.github.io/portfolio/#/'}
         />
-        
+        <ProjectCardComponent
+          image={'./assets/Oaxaca.PNG'}
+          title={'Oaxaca web app'}
+          description={'Restaurant web application for Customers and Staff written in React, GoLang and PostgreSQL'}
+          link={'https://github.com/Tworye01/Oaxaca-Restaurant-App'}
+        />
       </div>
 
       <NavigationBarComponent />
